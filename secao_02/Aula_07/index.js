150c7550bfc96072a7cac97960e8977f7bbe384c
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-14 22:21:44 -0300

    Aula_07

---
+++ secao_02/Aula_07/index.js
@@ -0,0 +1,11 @@
+// const nome = 'Gui';
+// nome = 'gds' // Vai dar erro
+// console.log(nome);
+
+const primeiroNumero = '5';
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+const resultadoTriplicado = resultado * 3;
+
+console.log(typeof (primeiroNumero + segundoNumero));

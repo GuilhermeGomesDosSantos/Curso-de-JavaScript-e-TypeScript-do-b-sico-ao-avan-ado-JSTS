8308edc334f2a1f61fd82529467d130d89c76478
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-15 16:40:49 -0300

    Aula 06

--- secao_02/Aula_06/index.js
+++ secao_02/Aula_06/index.js
@@ -1,4 +1,4 @@
-// let nome = "Guilherme";
+let nome = "Guilherme";
 
 // Não podemos criar variáveis com palavras reservadas, ex: let let, let if e etc
 // Não podemos redeclarar variaveis com let
@@ -12,14 +12,15 @@ nome = 'GDS'
 console.log(nome);
 */
 
+/*
 let nomeCliente = 'Guilherme'
 nomeCliente = 'GDS';
 
 console.log(nomeCliente);
-/*
+*/
+
 console.log(nome + " nasceu em 2003");
 console.log('Em 2023',nome,"conheceu Susana");
 console.log(nome + " casou-se com Susana em 2025");
 console.log('Susana teve 1 filho com o',nome,"em 2030");
 console.log('O filho de',nome,"se chama Adam");
-*/

b3bacc28ab2a158a6afb605335ec5623feb56764
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-16 17:58:55 -0300

    Aula 11

---
+++ secao_02/Aula_11/js/index.js
@@ -0,0 +1,7 @@
+let a = prompt('Digite um valor: ')
+let b = prompt('Digite outro valor: ')
+let r = Number (a) + Number (b)
+
+console.log(`Valor 1:${a}; Valor 2:${b}\nResultado é: ${r}`)
+
+alert('O Resultado é: ' + r)

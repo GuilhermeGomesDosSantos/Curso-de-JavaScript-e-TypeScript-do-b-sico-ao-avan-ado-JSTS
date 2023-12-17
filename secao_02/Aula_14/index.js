26f1ad312d9955ea1681850c0dd65d0332680d2b
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-17 20:35:09 -0300

    Aula 14

---
+++ secao_02/Aula_14/index.js
@@ -0,0 +1,41 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+// console.log(num1.toString() + num2); // converte para string
+// num1 = num1.toString(); // converte a variavel para string
+// console.log(typeof num1);
+
+// console.log(num1.toString(2)); // representação binaria do valor da variavel
+// console.log(num1.toFixed(2)); // arredonda o número da variavel em casas decimais
+// console.log(num1.toFixed(4));
+// console.log(Number.isInteger(num1)); // Verifica se o número é inteiro
+
+// let temp = num1 * 'Olá';
+// let temp = num1 * '5';
+// console.log(Number.isNaN(temp)); // verifica se a conta é verdadeira ou não
+
+/*
+num1 += num2
+num1 += num2
+num1 += num2
+
+num1 = num1.toFixed(2)
+console.log(num1);
+console.log(Number.isInteger(num1))
+*/
+/*
+num1 += num2
+num1 += num2
+num1 += num2
+
+num1 = parseFloat(num1.toFixed(2))
+console.log(num1);
+console.log(Number.isInteger(num1))
+*/
+
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+num1 = ((num1 * 100) + (num2 * 100)) / 100;
+
+console.log(num1);
+console.log(Number.isInteger(num1))

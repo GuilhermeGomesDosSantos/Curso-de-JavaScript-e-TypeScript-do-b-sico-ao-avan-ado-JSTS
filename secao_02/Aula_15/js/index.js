28b01ff1206d774553abe89cc3c34471609c8109
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-19 20:22:25 -0300

    Aula 15 Exercicio

--- secao_02/Aula_15/js/index.js
+++ secao_02/Aula_15/js/index.js
@@ -1,3 +1,12 @@
-const num = prompt('Digite um número: ')
+const num = Number (prompt('Digite um número: '));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
 
-document.body.innerHTML = `Raiz quadrada: ${Math.pow(num)}</br>`
+numeroTitulo.innerHTML = num;
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada: ${num ** 0.5}</p>`;
+texto.innerHTML += `<p>O número ${num} é inteiro: ${Number.isInteger(num)}</p>`
+texto.innerHTML += `<p>É NaN: ${Number.isNaN(num)}</p>`
+texto.innerHTML += `<p>Arredondamento para baixo: ${Math.floor(num)}</p>`
+texto.innerHTML += `<p>Arredondamento para cima: ${Math.ceil(num)}</p>`
+texto.innerHTML += `<p>Com duas casas decimais: ${(num.toFixed(2))}</p>`

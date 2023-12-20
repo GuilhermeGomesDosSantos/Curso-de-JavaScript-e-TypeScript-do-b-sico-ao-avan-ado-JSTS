4002ce98c25ba0ca68aeac698cb080ddd91782d3
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-19 21:48:52 -0300

    Aula 16

---
+++ secao_02/Aula_16/index.js
@@ -0,0 +1,46 @@
+// const alunos = ['Gui', 'Fulano', 1, true, null] // pode colocar qualquer coisa
+// const alunos = ['Gui', 'Fulano', 'joão'] // cada indice representa 1 elemento
+
+let alunos = ['Gui', 'Fulano', 'joão']
+alunos = 233;
+
+// console.log(alunos.length) // retorna o tamanho total de elementos
+// alunos[alunos.length] = 'Luiza'; // vai adicionando novos elementos
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+
+// console.log(alunos)
+// alunos[0] = 'Eduardo'; // alterando o valor do indice 0
+// alunos[2] = 'Luiza';
+// console.log(alunos)
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+
+// alunos.push('Otávio'); // adiciona novos elementos no final do Array
+// console.log(alunos)
+// alunos.unshift('GDS'); // vai colocar no indice 0 do Array
+// console.log(alunos);
+// alunos.pop(); // remove da ultima posicao
+// alunos.shift(); // remove do começo
+// console.log(alunos)
+
+// const removido = alunos.pop();
+// console.log(removido);
+// console.log(alunos);
+
+// delete alunos[1];
+// console.log(alunos[1]);
+
+// console.log(alunos[10]);
+
+// alunos.push('Zé')
+// alunos.push('Junior')
+
+// console.log(alunos);
+
+// console.log(alunos.slice(0, 2)) // seleciona quantos elementos quer que seja mostrado
+// console.log(alunos.slice(0, -2)) //
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);

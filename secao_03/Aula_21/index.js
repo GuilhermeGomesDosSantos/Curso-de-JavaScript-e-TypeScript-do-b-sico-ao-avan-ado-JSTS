ea114898b3aa3924a40b7102cba45140f8702b16
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-27 19:50:46 -0300

    Aula 21

---
+++ secao_03/Aula_21/index.js
@@ -0,0 +1,25 @@
+// Operadores de comparação
+
+// console.log(10 > 5); // tá pergunta se o 1° número é maior do que o 2°
+
+// const comp = 10 > 5;
+// console.log(comp);
+
+// const comp = 10 >= 5;
+// const comp = 10 >= 10;
+
+// const comp = 10 >= 11;
+// console.log(comp);
+
+// const comp = 10 < 11;
+// const comp = 12 < 11;
+// console.log(comp);
+
+const num1 = 11;
+const num2 = '11';
+// const comp = num1 < num2;
+// const comp = num1 == num2;
+// const comp = num1 === num2;
+// const comp = num1 != num2;
+const comp = num1 !== num2;
+console.log(comp);

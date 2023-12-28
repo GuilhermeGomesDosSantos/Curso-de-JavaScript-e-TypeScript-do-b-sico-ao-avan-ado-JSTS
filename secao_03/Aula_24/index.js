c5f7023475246ec256842cf8f7bf81c2fc27a298
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-27 22:09:33 -0300

    Aula 24

---
+++ secao_03/Aula_24/index.js
@@ -0,0 +1,23 @@
+/*
+const hora = 30;
+
+if (hora <= 0 && hora <= 11) {
+    console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde');
+} else if (hora >=18 && hora <= 23) {
+    console.log('Boa noite')
+} else {
+    console.log('Olá');
+}
+*/
+
+// const tenhoGrana = true;
+// const tenhoGrana = NaN;
+const tenhoGrana = 'Gui';
+
+if (tenhoGrana) {
+    console.log('Vou sair de casa');
+} else {
+    console.log('Não vou sair de casa');
+}

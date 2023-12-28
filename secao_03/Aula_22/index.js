b5a87afb460ade9a736e27c26a068e927c2e5f33
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2023-12-27 21:03:00 -0300

    Aula 22

---
+++ secao_03/Aula_22/index.js
@@ -0,0 +1,46 @@
+/*
+Operadores Lógicos
+
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+
+/* AND &&
+console.log(true && true);
+console.log(true && true && true && true);
+console.log(true && true && true && true && false);
+const expressao = true && true && false
+console.log(expressao)
+*/
+
+/* OR ||
+const expressao = true || true || false;
+console.log(true || false || false);
+console.log(false || false || false);
+console.log(expressao)
+*/
+
+/*
+const usuario = 'Gui'
+const senha = 123;
+// const vaiLogar = usuario === 'Gui' && senha === 123;
+// const vaiLogar = usuario === 'Gui' && senha === 1234;
+const vaiLogar = usuario === 'Gui' || senha === 1234;
+console.log(vaiLogar);
+*/
+
+console.log(true);
+console.log(false);
+console.log(!true); // ! inverte o valor
+console.log(!false);
+console.log(!!!!!false);
+console.log(!!!!!!!!!!false);
+
+
+
+
+
+
+
+

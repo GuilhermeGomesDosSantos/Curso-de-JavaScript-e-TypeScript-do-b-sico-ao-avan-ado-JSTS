8722196541d001d73e762be7a1085cfd9ebea6c9
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-01-06 13:22:11 -0300

    Aula 34

---
+++ secao_03/Aula_34/index.js
@@ -0,0 +1,46 @@
+/*
+for (let i = 0; i <= 5; i++) {// i significa index
+    // console.log(i);
+    console.log(`Linha ${i}`);
+}
+*/
+
+/*
+for (let i = 400; i <= 500; i++) {
+    console.log(`Linha ${i}`);
+}
+*/
+
+/*
+for (let i = 400; i <= 500; i += 10) {
+    console.log(`Linha ${i}`);
+}
+*/
+
+/*
+for (let i = 500; i >= 400; i -= 10) {
+    console.log(`Linha ${i}`);
+}
+*/
+
+/*
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0;
+    console.log(i, par);
+}
+*/
+
+/*
+for (let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'Par' : 'Ímpar';
+
+    console.log(i, par);
+}
+*/
+
+const frutas = ['Maçã', 'Pêra','Uva', 'Manga', 'Açai', 'Banana'];
+for (let i = 0;i  < frutas.length; i++) {
+    // console.log(i);
+    // console.log(`Frutas ${frutas[i]}`);
+    console.log(`Índice ${i}`, frutas[i]);
+}
